f12ac23e30e285ef68878abdd5d05f1ee5224c79
DOUGLAS  FERREIRA <98502897+Douglasferreira06@users.noreply.github.com>
2024-06-21 22:59:56 -0300

    commit

---
+++ script.js
@@ -0,0 +1,97 @@
+/*let inputAdultos = document.getElementById("Adulto")
+let inputCrianca = document.getElementById("Criança")
+let inputDuracao = document.getElementById("Duração")
+
+let resulatdo = document.getElementById("Resultado")
+
+
+function calcular (){  
+
+    console.log("calcular ...")
+
+let Adulto = inputAdultos.value
+let Criança = inputCrianca.value
+let Duração = inputDuracao.value
+
+let qtdTotalCarne = carnePP(Duração)* Adulto +(carnePP (Duração)) /2 *Criança;
+let qtdTotalCerveja = cervejaPP (Duração)*Adulto;
+let qtdTotalBebidas= bebidasPP(Duração)* Adulto +(bebibasPP (Duração)) /2 *Criança;
+
+resulatdo.innerHTML = `<p> ${qtdTotalCarne}g de Carne </p>`
+resulatdo.innerHTML += `<p> ${qtdTotalCerveja}Ml de Cerveja </p>`
+resulatdo.innerHTML += `<p> ${qtdTotalBebidas}ml de Bebidas </p>`
+
+}
+
+    function carnePP ( Duração){
+
+        if(Duração >= 6)
+            return 650;
+        else{
+            return 400;
+        }
+    }
+
+    
+    function cervejaPP (Duração){
+
+        if(Duração >= 6)
+            return 2000;
+        else{
+            return 1200;
+        }
+    }
+    function bebidasPP (Duração){
+
+        if(Duração >= 6)
+            return 1500;
+        else{
+            return 1000;
+        }
+    }*/
+
+        let inputAdultos = document.getElementById("Adulto");
+let inputCrianca = document.getElementById("Crianca");
+let inputDuracao = document.getElementById("Duracao");
+
+let resultado = document.getElementById("resultado");
+
+function calcular() {
+    console.log("calcular ...");
+
+    let Adulto = parseInt(inputAdultos.value);
+    let Crianca = parseInt(inputCrianca.value);
+    let Duracao = parseInt(inputDuracao.value);
+
+    let qtdTotalCarne = carnePP(Duracao) * Adulto + (carnePP(Duracao) / 2 * Crianca);
+    let qtdTotalCerveja = cervejaPP(Duracao) * Adulto;
+    let qtdTotalBebidas = bebidasPP(Duracao) * Adulto + (bebidasPP(Duracao) / 2 * Crianca);
+
+    resultado.innerHTML = `<p>${qtdTotalCarne}g de Carne</p>`;
+    resultado.innerHTML += `<p>${qtdTotalCerveja}ml de Cerveja</p>`;
+    resultado.innerHTML += `<p>${qtdTotalBebidas}ml de Bebidas</p>`;
+}
+
+function carnePP(Duracao) {
+    if (Duracao >= 6) {
+        return 650;
+    } else {
+        return 400;
+    }
+}
+
+function cervejaPP(Duracao) {
+    if (Duracao >= 6) {
+        return 2000;
+    } else {
+        return 1200;
+    }
+}
+
+function bebidasPP(Duracao) {
+    if (Duracao >= 6) {
+        return 1500;
+    } else {
+        return 1000;
+    }
+}
